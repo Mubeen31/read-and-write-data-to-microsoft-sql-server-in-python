ce9ac0022c3eb554185b7036d2233f5b0e0d815b
Mubeen Ali <qs6272527@gmail.com>
2022-11-27 12:03:50 +0000

    first commit

--- sqlalchemy-read-table.py
+++ sqlalchemy-read-table.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-server_name = 'sensorstemphum.database.windows.net'
-database_name = 'weatherdata'
-user_name = 'temphum'
-password = 'Temp!hum'
+server_name = ''
+database_name = ''
+user_name = ''
+password = ''
 
-eng = create_engine('mssql+pyodbc://'+user_name+':'+password+'@'+server_name+'/weatherdata?driver=SQL+Server')
+eng = create_engine('mssql+pyodbc://'+user_name+':'+password+'@'+server_name+'/type database name here?driver=SQL+Server')
 
 df = pd.read_sql_table('Customer', eng, columns = ['CustomerID',
                                                    'CustomerName',
