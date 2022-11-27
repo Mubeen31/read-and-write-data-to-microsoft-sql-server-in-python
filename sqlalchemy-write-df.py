f00d7191e2606a3971e9812cbb8b4429579e6e58
Mubeen Ali <qs6272527@gmail.com>
2022-11-27 12:05:06 +0000

    first commit

--- sqlalchemy-write-df.py
+++ sqlalchemy-write-df.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-server_name = 'sensorstemphum.database.windows.net'
-database_name = 'weatherdata'
-user_name = 'temphum'
-password = 'Temp!hum'
+server_name = ''
+database_name = ''
+user_name = ''
+password = ''
 
 df = pd.read_csv('customer.csv')
 df.drop('Unnamed: 0', inplace = True, axis = 1)
 
-eng = create_engine('mssql+pyodbc://'+user_name+':'+password+'@'+server_name+'/weatherdata?driver=SQL+Server')
+eng = create_engine('mssql+pyodbc://'+user_name+':'+password+'@'+server_name+'/type database name here?driver=SQL+Server')
 # No need to design table in Microsoft SQL server management studio.
 # sqlalchemy will create and design self table.
 df.to_sql('Customer', eng, if_exists = 'append', index = False)
